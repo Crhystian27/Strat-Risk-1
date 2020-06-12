dbd8059fa455fbd29a01fc4f06631f9b2943ac42
Cristian David <cdavid2127@gmail.com>
2020-06-12 13:56:11 -0500

    update

---
+++ app/src/main/java/co/mba/strat_risk/di/key/ViewModelKey.java
@@ -0,0 +1,18 @@
+package co.mba.strat_risk.di.key;
+
+
+import androidx.lifecycle.ViewModel;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import dagger.MapKey;
+
+
+public interface ViewModelKey {
+    Class<? extends ViewModel> value();
+}
+
