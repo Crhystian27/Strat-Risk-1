77fd9716843e547a1761244b576b31292e23184b
crhystian <cdavid2127@gmail.com>
2020-07-27 12:25:36 -0500

    New Login and custom EditText, CheckBox and ButtonBar Login

--- app/src/main/java/co/mba/strat_risk/ui/login/LoginFragment.java
+++ app/src/main/java/co/mba/strat_risk/ui/login/LoginFragment.java
@@ -1,5 +1,6 @@
 package co.mba.strat_risk.ui.login;
 
+import androidx.cardview.widget.CardView;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
 
@@ -9,6 +10,10 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.checkbox.MaterialCheckBox;
+import com.google.android.material.textfield.TextInputEditText;
 
 import javax.inject.Inject;
 
@@ -32,6 +37,33 @@ public class LoginFragment extends BaseFragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         viewModel = ViewModelProviders.of(this, factory).get(LoginFragmentViewModel.class);
+    }
+
+    private void initUi(View view) {
+
+        MaterialCheckBox materialCheckBox = view.findViewById(R.id.login_checkbox);
+        CardView cardView = view.findViewById(R.id.login_button);
+        TextInputEditText username = view.findViewById(R.id.login_username);
+        TextInputEditText password = view.findViewById(R.id.login_password);
+
+        cardView.setOnClickListener(v -> {
+
+            if (materialCheckBox.isChecked()) {
+
+                if (username.toString().isEmpty() || password.toString().isEmpty()) {
+                    Toast.makeText(getBaseActivity(), "show dialog", Toast.LENGTH_LONG).show();
+
+                } else {
+                    //TODO CALL VIEWMODEL AND SEDN DATA
+
+                }
+
+
+            } else {
+                Toast.makeText(getBaseActivity(), "show dialog", Toast.LENGTH_LONG).show();
+            }
+
+        });
 
 
     }
